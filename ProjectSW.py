a14de0019f218e9156754b8cfdb7e69ea784c215
pmotakef <pmotakef@umd.edu>
2017-04-23 11:52:17 -0400

    Updated File reading  and collocation computation
    Updated File reading  and collocation computation

--- ProjectSW.py
+++ ProjectSW.py
@@ -14,8 +14,8 @@ from os.path import join,isfile
 import pickle
 from itertools import chain
 
-controlsPath = "controls/suicidewatch_controls/"
-positivesPath = "positives/suicidewatch_positives/"
+controlsPath = "controls/"
+positivesPath = "positives/"
 liwcPath = "other_materials/liwc/"
 picklePath = 'tmp/'
 
@@ -34,9 +34,9 @@ LIWC_words = {}
 PostsStruct = namedtuple("PostsStruct", "postID userID timeStamp subReddit postTitle postBody")
 
 stopws = stopwords.words('english')
-# TODO: does strong.punctuation need to be converted to unicode?
+# TODO: does string.punctuation need to be converted to unicode?
 stopws.extend(string.punctuation)
-stopws.extend(u"'s 're n't 'm 've --".split())
+stopws.extend(u"'s 're n't 'm 've 'd '' 't --".split())
 
 ###
 # File I/O
@@ -70,6 +70,9 @@ def readPosts(filename, users=None):
                 posts.append(ps)
     return posts
 
+def getPostFilename(userID):
+    return str(abs(math.floor(userID / 2000.0))).split('.')[0]
+
 def loadPosts(posUsers, negUsers):
     """
     Loads all posts from a set of positive users and negative users
@@ -85,15 +88,35 @@ def loadPosts(posUsers, negUsers):
     posPosts = []
     negPosts = []
 
+    postFilenames = []
+
     # Read positive posts (filenames: 0.txt...25.txt)
-    for i in xrange(26):
-        posFilename = join(positivesPath, str(i) + '.posts')
+    for puid in posUsers:
+        tmp = getPostFilename(puid)
+        if tmp not in postFilenames:
+            postFilenames += [tmp]
+    for filename in postFilenames:
+        posFilename = join(positivesPath, filename + '.posts')
         posPosts += readPosts(posFilename, posUsers)
 
+    #for i in xrange(26):
+    #    posFilename = join(positivesPath, str(i) + '.posts')
+    #    posPosts += readPosts(posFilename, posUsers)
+
     # Read control posts (filenames: 1.txt...31.txt)
-    for i in xrange(1,32):
-        negFilename = join(controlsPath, str(i) + '.posts')
+    postFilenames = []
+
+    for nuid in negUsers:
+        tmp = getPostFilename(nuid)
+        if tmp not in postFilenames:
+            postFilenames += [tmp]
+    for filename in postFilenames:
+        negFilename = join(controlsPath, filename + '.posts')
         negPosts += readPosts(negFilename, negUsers)
+
+    #for i in xrange(1,32):
+    #    negFilename = join(controlsPath, str(i) + '.posts')
+    #    negPosts += readPosts(negFilename, negUsers)
 
     # Write to pickle cache
     with open(pickle_filename, 'wb') as f:
@@ -202,7 +225,7 @@ def calcAverageD(p, q, v):
 # Collocation Feature Exploration
 ###
 
-def collocations():
+def collocations(ngram, tokenizedPosPosts, tokenizedNegPosts):
     """
     Generates all bigram collocations and computes their KL-divergence
     """
@@ -211,30 +234,43 @@ def collocations():
 
     bigram_measures = nltk.collocations.BigramAssocMeasures()
     wordnet_lemmatizer = WordNetLemmatizer()
+    vocab = {}
 
     # Count n-grams
-    for uid in tpPosts:
-        #finder = BigramCollocationFinder.from_words(tpPosts[p])
-        #finder.apply_word_filter(lambda w: w in stopws)
-        tokens = (t for t in tpPosts[uid] if t not in stopws)
-        monogram = nltk.FreqDist(tokens).items()
-        print monogram
-        #collections.Counter(tpPosts[uid])
-        allBigrams[("pos", uid)] = monogram #finder.ngram_fd.items()
+    for uid in tokenizedPosPosts:
+        vocab[uid] = []
+        if (ngram == 1):
+            tokens = (t for t in tokenizedPosPosts[uid] if t not in stopws)
+            gram = nltk.FreqDist(tokens).items()
+        else:
+            finder = BigramCollocationFinder.from_words(tokenizedPosPosts[uid])
+            finder.apply_word_filter(lambda w: w in stopws)
+            gram = finder.ngram_fd.items()
+        for (w, v) in gram:
+            if (w not in vocab[uid]):
+                vocab[uid] += [w]
+        allBigrams[("pos", uid)] = gram
 
-    for uid in tnPosts:
-        #finder = BigramCollocationFinder.from_words(tnPosts[uid])
-        #finder.apply_word_filter(lambda w: w in stopws)
-        tokens = (t for t in tnPosts[uid] if t not in stopws)
-        monogram = nltk.FreqDist(tokens).items()
-        allBigrams[("neg", uid)] = monogram #finder.ngram_fd.items()
+    for uid in tokenizedNegPosts:
+        vocab[uid] = []
+        if (ngram == 1):
+            tokens = (t for t in tokenizedNegPosts[uid] if t not in stopws)
+            gram = nltk.FreqDist(tokens).items()
+        else:
+            finder = BigramCollocationFinder.from_words(tokenizedNegPosts[uid])
+            finder.apply_word_filter(lambda w: w in stopws)
+            gram = finder.ngram_fd.items()
+        for (w, v) in gram:
+            if (w not in vocab[uid]):
+                vocab[uid] += [w]
+        allBigrams[("neg", uid)] = gram
 
     # Build`token vocabulary
-    vocab = []
-    for bigrams in allBigrams.values():
-        for token, count in bigrams:
-            if (token not in vocab):
-                vocab.append(token)
+    #vocab = {}
+    #for bigrams in allBigrams.values():
+    #    for token, count in bigrams:
+    #        if (token not in vocab):
+    #            vocab.append(token)
 
     print "Calculating D values."
     Mt = {}
@@ -245,19 +281,16 @@ def collocations():
             elif (uid2, uid1) in Mt:
                 ansD = Mt[(uid2, uid1)]
             else:
-                Mt[(uid1, uid2)] = calcAverageD(allBigrams[(class1,uid1)], allBigrams[(class2,uid2)], vocab)
+                Mt[(uid1, uid2)] = calcAverageD(allBigrams[(class1,uid1)], allBigrams[(class2,uid2)], vocab[uid1] + vocab[uid2])
                 ansD = Mt[(uid1,uid2)]
             print "D (" + str(uid1) + " || " + str(uid2) + " ) = " + str(ansD)
+    return Mt
 
-    """temp = readPost(positivesPath+"0.posts")
-    for i in range(0,2):
-        print temp[i].postBody
-    loadLIWC()
-    print LIWC_words['ace']
-    print getLIWCclass('kissed')
-    print translateAllLIWCClasses(getLIWCclass('kissing'))
-    print translateAllLIWCClasses(getLIWCclass('a'))
-    """
+def saveCollocations(filename, tableM):
+    with open(filename, "w") as f:
+        for (uid1,uid2) in tableM:
+            line = str(uid1) + '\t' + str(uid2) + '\t' + str(tableM[(uid1, uid2)]) + '\n'
+            f.write(line)
 
 if __name__ == "__main__":
     random.seed(773)
@@ -270,11 +303,21 @@ if __name__ == "__main__":
     print 'Control Samples: %s' % negSamples
 
     print "Reading posts."
-    # TODO: Only load the files that contain the sampled user ids
-    #       file = abs(floor(uid/2000)) + ".posts"
+
     posPosts,negPosts = loadPosts(posSamples, negSamples)
 
     tpPosts = tokenizePosts(concatPostsByUser(posPosts))
     tnPosts = tokenizePosts(concatPostsByUser(negPosts))
 
-    collocations()
+    saveCollocations("results/dlconv.txt", collocations(2, tpPosts, tnPosts))
+
+
+    """temp = readPost(positivesPath+"0.posts")
+    for i in range(0,2):
+        print temp[i].postBody
+    loadLIWC()
+    print LIWC_words['ace']
+    print getLIWCclass('kissed')
+    print translateAllLIWCClasses(getLIWCclass('kissing'))
+    print translateAllLIWCClasses(getLIWCclass('a'))
+    """
